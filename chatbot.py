ef426ef0a558330c399eb0b852d009d619544a19
kankit08 <kankit08@gmail.com>
2020-09-10 17:36:55 +0530

    message

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,11 @@ from nltk.chat.util import Chat, reflections
 
 pairs = [
     ['my name is (.*)',['hi %1']],
-    ['(hi |hello |hey| holla| hola)',['hey there']]
+    ['(hi |hello |hey| holla| hola)',['hey there']],
+    ['(.*) in (.*) is fun', ['%1 in %2 is indeed a fun']],
+    ['(.*) (location|city) ?', 'New Delhi, India'],
+    ['(.*) created you  ?', ['Ankit did using NLTK']]
+    ['how is the weather  in (.*)',['the weather in %1 is amazing like always']]
     ]
 chat = Chat(pairs, reflections)
 print("Here is the chatbot: ")
